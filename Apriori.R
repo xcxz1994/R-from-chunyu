c90523e25b35d1b1a76d0f06317e994cb31bea88
SPring Run <chunyuchunyu2015@outlook.com>
2017-05-29 22:16:07 +0800

    Update Apriori.R

--- Apriori.R
+++ Apriori.R
@@ -1,12 +1,9 @@
-firstdata<-c(c("I1","I2","I5"),c("I2","I4"),c("I2","I3"),c("I1","I2","I4"),c("I1","I3"),c("I2","I3"),c("I1","I3"),c("I1","I2","I3","I5"),c("I1","I2","I3"))
-datajihe<-factor(firstdata)
-datajihe
+firstdata<-list(c("I1","I2","I5"),c("I2","I4"),c("I2","I3"),c("I1","I2","I4"),c("I1","I3"),c("I2","I3"),c("I1","I3"),c("I1","I2","I3","I5"),c("I1","I2","I3"))
+datajihe<-factor(unlist(firstdata))
 lev<-levels(datajihe)
-lev
 n<-(length(firstdata))
 smlist<-list(xiangji="",zhicidu="")
 lenjihe<-length(datajihe)
-lenjihe
 zcdcount<-function(datajihe){
   lenlev<-length(lev)
   for(i in 1:lenjihe){
@@ -24,8 +21,38 @@ zcdcount<-function(datajihe){
 saomiao<-zcdcount(datajihe)
 saomiao$xiangji<-saomiao$xiangji[-length(saomiao$xiangji)]
 saomiao$zhicidu<-saomiao$zhicidu[-length(saomiao$zhicidu)]
-saomiao
-houxuanji<-combn(lev,2)
+saomiao$zhicidu
+houxuanji<-combn(saomiao$xiangji,2)
+c2<-function(){
+     for(i in 1: ncol(houxuanji))
+      {
+          count<-0
+          x<-houxuanji[,i]
+          for(j in 1: n){
+            if(sum(x %in% firstdata[[j]])==length(x)){
+              count<-count+1
+            }
+          }
+          if(count>=2){
+            smlist$xiangji<-append(houxuanji[,i],smlist$xiangji,after = 10)
+            smlist$zhicidu<-append(count,smlist$zhicidu,after = 10)
+          }
+     }
+  return(smlist)
+}
+saomiaoc2<-c2()
+saomiaoc2$xiangji<-saomiaoc2$xiangji[-length(saomiaoc2$xiangji)]
+saomiaoc2$zhicidu<-saomiaoc2$zhicidu[-length(saomiaoc2$zhicidu)]
+l3<-seq(1,length(saomiaoc2$xiangji),by=2)
+c3<-function(){
+  for(i in l3){
+    houxuanc3<-union(saomiaoc2$xiangji[i],saomiaoc2$xiangji[i+1])
+    print(houxuanc3)
+  }
+  
+}
+c3()
+
 
 
 
